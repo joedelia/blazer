d004346ae57b738b6148dabbeaaf361e289b8d33
Joe d'Elia <joe@finestventures.com>
2019-03-20 11:43:55 +0000

    Update run_statement_job.rb

--- lib/blazer/run_statement_job.rb
+++ lib/blazer/run_statement_job.rb
@@ -3,9 +3,7 @@ module Blazer
     def perform(data_source_id, statement, options)
       data_source = Blazer.data_sources[data_source_id]
       begin
-        ActiveRecord::Base.connection_pool.with_connection do
-          Blazer::RunStatement.new.perform(data_source, statement, options)
-        end
+        Blazer::RunStatement.new.perform(data_source, statement, options)
       rescue Exception => e
         Blazer::Result.new(data_source, [], [], "Unknown error", nil, false)
         Blazer.cache.write(data_source.run_cache_key(options[:run_id]), Marshal.dump([[], [], "Unknown error", nil]), expires_in: 30.seconds)
